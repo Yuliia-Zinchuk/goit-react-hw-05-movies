eab83899876da77f71f3e014a13786c34e0a4e6e
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-10-22 03:39:09 +0300

    Create Layout.jsx

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,11 @@
+import { Navigation } from 'components/Navigation/Navigation';
+import { Outlet } from 'react-router-dom';
+
+export const Layout = () => {
+  return (
+    <>
+      <Navigation />
+      <Outlet />
+    </>
+  );
+};
