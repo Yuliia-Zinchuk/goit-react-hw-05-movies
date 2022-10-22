4257d3003d9f957762ff34c0f5be9c0b7d793af3
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-10-22 03:35:07 +0300

    add navigation+style

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,18 @@
+//import { NavLink } from 'react-router-dom';
+import { StyledLink } from './Navigation.styled';
+export const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <StyledLink to="/" end>
+            Home
+          </StyledLink>
+        </li>
+        <li>
+          <StyledLink to="movies">Movies</StyledLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
---
+++ src/components/Navigation/Navigation.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+export const StyledLink = styled(NavLink)`
+text-decoration: none;
+font-size 18px;
+color: blue;
+
+&.active {
+    color: red;
+}
+`;
