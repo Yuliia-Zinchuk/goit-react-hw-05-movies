e1791126ab83fdb268500b5ed5f9f848ced009ef
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-10-23 14:24:42 +0300

    Update ReviewsPage.jsx

--- src/pages/ReviewsPage/ReviewsPage.jsx
+++ src/pages/ReviewsPage/ReviewsPage.jsx
@@ -1,3 +1,30 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchMovieReviews } from 'services/fetchApi';
+
 export const ReviewsPage = () => {
-  return <div>Cast!!! Нужно сделать фетч и сюда прислать инфу reviews</div>;
+  const [review, setReview] = useState([]);
+  const { movieId } = useParams();
+  useEffect(() => {
+    fetchMovieReviews(movieId).then(setReview);
+  }, [movieId]);
+  console.log(review);
+  return (
+    <>
+      {review ? (
+        <ul>
+          {review.map(({ id, author, content }) => {
+            return (
+              <li key={id}>
+                <h3>{author}</h3>
+                <p>{content}</p>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p>сделать компонент на ошибку и активировать его </p>
+      )}
+    </>
+  );
 };
