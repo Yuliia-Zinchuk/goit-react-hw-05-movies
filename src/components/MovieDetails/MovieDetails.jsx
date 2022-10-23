0ffd769ce34a2ef3983dcfbba657315d23b74d14
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-10-23 14:19:51 +0300

    add button Go back

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,34 +1,54 @@
 import { Link, Outlet } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom/dist';
 
 export const MovieDetails = ({ movie }) => {
+  const navigate = useNavigate();
   return (
-    movie && (
-      <>
-        <div>
-          <h2>
-            {movie.original_title} ({`${movie.release_date}`.slice(0, 4)})
-          </h2>
-          <p>Use score: {movie.popularity}</p>
-          <h3>Overview</h3>
-          <p>{movie.overview}</p>
-          <h3>Overview</h3>
-          <p>{movie.genres[1].name}</p>
-        </div>
-        <div>
-          <p>Additional information</p>
-          <nav>
-            <ul>
-              <li>
-                <Link to="cast">Cast</Link>
-              </li>
-              <li>
-                <Link to="reviews">Reviews</Link>
-              </li>
-            </ul>
-            <Outlet />
-          </nav>
-        </div>
-      </>
-    )
+    <>
+      {movie && (
+        <>
+          <button
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            Go back
+          </button>
+          <div>
+            <img
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
+              width="400"
+            />
+            <h2>
+              {movie.title} ({`${movie.release_date}`.slice(0, 4)})
+            </h2>
+            <p>Use score: {Math.round(movie.vote_average * 10)}%</p>
+            <h3>Overview</h3>
+            <p>{movie.overview}</p>
+            <h3>Genres</h3>
+            <p>
+              {movie.genres.map(({ id, name }) => {
+                return <span key={id}>{name} </span>;
+              })}
+            </p>
+          </div>
+          <div>
+            <p>Additional information</p>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="cast">Cast</Link>
+                </li>
+                <li>
+                  <Link to="reviews">Reviews</Link>
+                </li>
+              </ul>
+              <Outlet />
+            </nav>
+          </div>
+        </>
+      )}
+    </>
   );
 };
